9ae4f1c78a10c7ea75ad75aa48bc7cb37ce8620a
Jeong Min Lee <jacklee0223@gmail.com>
2020-01-07 00:43:13 -0600

    implement pyramid recursive

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,22 +14,44 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {
+// function pyramid(n) {
+//   const mid = Math.floor((2 * n - 1) / 2);
+
+//   for (let row = 0; row < n; row++) {
+//     let level = '';
+
+//     for (let column = 0; column < 2 * n - 1; column++) {
+//       if (mid - row <= column && mid + row >= column) {
+//         level += '#';
+//       } else {
+//         level += ' ';
+//       }
+//     }
+
+//     console.log(level);
+//   }
+// }
+
+function pyramid(n, row = 0, level = '') {
+  if (n === row) return;
+
+  if (2 * n - 1 === level.length) {
+    console.log(level);
+    return pyramid(n, row + 1);
+  }
+
   const mid = Math.floor((2 * n - 1) / 2);
 
-  for (let row = 0; row < n; row++) {
-    let level = '';
+  // if (mid - row <= level.length && mid + row >= level.length) {
+  //   level += '#';
+  // } else {
+  //   level += ' ';
+  // }
 
-    for (let column = 0; column < 2 * n - 1; column++) {
-      if (mid - row <= column && mid + row >= column) {
-        level += '#';
-      } else {
-        level += ' ';
-      }
-    }
+  const addCondition = mid - row <= level.length && mid + row >= level.length;
+  const add = addCondition ? (level += '#') : (level += ' ');
 
-    console.log(level);
-  }
+  pyramid(n, row, level);
 }
 
 module.exports = pyramid;
