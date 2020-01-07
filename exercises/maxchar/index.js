a9dc156c1c178047f8f5e8ce5cc929bfb8c4038a
Jeong Min Lee <jacklee0223@gmail.com>
2020-01-06 19:46:28 -0600

    implement maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,45 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+// function maxChar(str) {
+//   const charMap = {};
+//   let max = 0;
+//   let maxChar = '';
+
+//   for (let char of str) {
+//     if (charMap[char]) {
+//       charMap[char]++;
+//     } else {
+//       charMap[char] = 1;
+//     }
+//   }
+
+//   for (let char in charMap) {
+//     if (charMap[char] > max) {
+//       max = charMap[char];
+//       maxChar = char;
+//     }
+//   }
+
+//   return maxChar;
+// }
+
+function maxChar(str) {
+  const charMap = {};
+  let max = 0;
+  let maxChar = '';
+
+  for (let char of str) {
+    if (charMap[char]) {
+      charMap[char]++;
+    } else {
+      charMap[char] = 1;
+    }
+  }
+
+  return Object.keys(charMap).reduce((prev, curr) =>
+    charMap[curr] > prev ? curr : prev
+  );
+}
 
 module.exports = maxChar;
