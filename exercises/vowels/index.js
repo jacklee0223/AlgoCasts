7e4602954cfa7c2476bbe5c78d7c0ce14f0d93f8
Jeong Min Lee <jacklee0223@gmail.com>
2020-01-07 00:56:22 -0600

    implement vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,30 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+// function vowels(str) {
+//   const vowelMap = ['a', 'e', 'i', 'o', 'u'];
+//   let count = 0;
+
+//   for (char of str.toLowerCase()) {
+//     if (vowelMap.includes(char)) count++;
+//   }
+
+//   return count;
+// }
+
+// function vowels(str, count = 0) {
+//   const matches = str.match(/[aeiou]/gi);
+//   return matches ? matches.length : 0;
+// }
+
+function vowels(str, count = 0) {
+  if (str.length < 1) return count;
+
+  const vowelMap = ['a', 'e', 'i', 'o', 'u'];
+  const first = str[0].toLowerCase();
+  const add = vowelMap.includes(first) ? 1 : 0;
+
+  return vowels(str.slice(1), count + add);
+}
 
 module.exports = vowels;
