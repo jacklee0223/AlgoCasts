7995ffba09ff9b02055f44d946fd72590a53221b
Jeong Min Lee <jacklee0223@gmail.com>
2020-01-07 00:11:53 -0600

    implement steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,40 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// function steps(n) {
+//   for (let row = 0; row < n; row++) {
+//     let stair = '';
+
+//     for (let column = 0; column < n; column++) {
+//       column <= row ? (stair += '#') : (stair += ' ');
+//     }
+//     console.log(stair);
+//   }
+// }
+
+function steps(n, row = 0, stair = '') {
+  // if (row === n) then we have hit the end of our problem
+  if (n === row) {
+    return;
+  }
+
+  // if the stair string has a length === n then we are at the
+  // end of a row
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row + 1);
+  }
+
+  // if the length of the stair string is less than or equal to the
+  // row number we're working on, we add a #, otherwise add
+  // a space
+  if (stair.length <= row) {
+    stair += '#';
+  } else {
+    stair += ' ';
+  }
+
+  steps(n, row, stair);
+}
 
 module.exports = steps;
